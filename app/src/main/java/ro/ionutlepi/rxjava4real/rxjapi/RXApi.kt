23440733eca2d6d992431977a182d6bd1f172df9
Ionut Lepadatescu <onealepadatescu@gmail.com>
2018-11-04 14:20:35 +0200

    Our first rx approach

---
+++ app/src/main/java/ro/ionutlepi/rxjava4real/rxjapi/RXApi.kt
@@ -0,0 +1,41 @@
+package ro.ionutlepi.rxjava4real.rxjapi
+
+import io.reactivex.Completable
+import io.reactivex.Single
+import ro.ionutlepi.rxjava4real.ResourceManager
+import ro.ionutlepi.rxjava4real.api.ApiInstance
+import ro.ionutlepi.rxjava4real.api.CallResult
+import ro.ionutlepi.rxjava4real.api.Callback
+import ro.ionutlepi.rxjava4real.api.IotDevice
+import ro.ionutlepi.rxjava4real.api.calls.ApiAuth
+import ro.ionutlepi.rxjava4real.api.calls.ApiCaller
+import ro.ionutlepi.rxjava4real.api.calls.SearchForDevice
+
+class RXApi(
+    private val apiInstance: ApiInstance = ApiInstance()
+) {
+
+    private fun createSingleForCall(apiCaller: ApiCaller): Single<CallResult> {
+        return Single.create<CallResult> { emmiter ->
+            apiCaller.execute(object : Callback {
+                override fun onCallFinished(result: CallResult) {
+                    if (emmiter.isDisposed) {
+                        //nothing to do if disposed
+                        return
+                    }
+                    if (result.successful) {
+                        emmiter.onSuccess(result)
+                    } else {
+                        emmiter.onError(CallError())
+                    }
+                }
+            })
+        }
+    }
+
+    fun auth(): Completable = createSingleForCall(ApiAuth(apiInstance)).ignoreElement()
+
+    fun search(deviceRef: String): Single<IotDevice> = auth()
+        .andThen(createSingleForCall(SearchForDevice(apiInstance, deviceRef)))
+        .map { it.iotDevice }
+}
